e0f948443040b9f74caad3853ecc9a698b674fdf
Jhonatan-Diniz <102447357+Jhonatan-Diniz@users.noreply.github.com>
2024-04-17 17:12:53 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,1 @@
-function reveal(name) {
-  let text_area = document.querySelector(`#${name}`)
-  let btn = document.getElementsByName(`${name}`)
 
-  console.log(btn)
-  if (text_area.style.display == "flex"){
-    text_area.style.display = ""
-    btn.innerText = ">"
-  } else {
-    text_area.style.display = 'flex'
-    btn.innerText = "<"
-  }
-}
-
-addEventListener()
